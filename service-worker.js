a7d7d587dd5dfbec7e47795debf4d40079ea09d0
Chu Đức Hiếu <hiu2003dz@gmail.com>
2026-04-25 14:14:31 +0700

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,22 @@
+const CACHE_NAME = 'vsp-cc-cache-v12';
+const urlsToCache = [
+  './',
+  './index.html',
+  './manifest.json'
+];
+
+// Cài đặt và lưu giao diện vào máy
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+// Trả về giao diện từ máy khi mất mạng thay vì báo lỗi
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
