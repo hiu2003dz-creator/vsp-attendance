1e37de617bdf4373cadf6efd19d51400656fc29a
Chu Đức Hiếu <hiu2003dz@gmail.com>
2026-06-06 23:46:01 +0700

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
 // Nâng version lên v21 để ép điện thoại nạp quy tắc mới
-const CACHE_NAME = 'vsp-cc-cache-v34'; 
+const CACHE_NAME = 'vsp-cc-cache-v36'; 
 const urlsToCache = [
   './',
   './index.html',
